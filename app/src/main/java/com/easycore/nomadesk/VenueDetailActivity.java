70c247964142757b4ce19598646e6189400b0d90
Michal Kejzlar <kejzlarmichal@gmail.com>
2016-10-19 23:32:00 +0100

    New resources from Plaid

--- app/src/main/java/com/easycore/nomadesk/VenueDetailActivity.java
+++ app/src/main/java/com/easycore/nomadesk/VenueDetailActivity.java
@@ -2,19 +2,13 @@ package com.easycore.nomadesk;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class VenueDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_venue_detail);
-
-        final View mContentView = findViewById(R.id.fullscreen_content);
-
-        assert mContentView != null;
 
     }
 
