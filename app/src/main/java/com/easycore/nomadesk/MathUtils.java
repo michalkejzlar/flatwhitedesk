0992767780b5685c6ddc5bb1398823be696b755d
Michal Kejzlar <kejzlarmichal@gmail.com>
2016-10-19 21:16:35 +0100

    Prepare widgets

---
+++ app/src/main/java/com/easycore/nomadesk/MathUtils.java
@@ -0,0 +1,11 @@
+package com.easycore.nomadesk;
+
+
+public class MathUtils {
+
+    private MathUtils() { }
+
+    public static float constrain(float min, float max, float v) {
+        return Math.max(min, Math.min(max, v));
+    }
+}
