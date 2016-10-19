0ef78eada76477b0086b01f283683242e2adfc78
Jakub Begera <jakub.begera@gmail.com>
2016-10-19 21:12:07 +0100

    libs added

---
+++ app/src/main/java/com/easycore/nomadesk/NomadeskApp.java
@@ -0,0 +1,14 @@
+package com.easycore.nomadesk;
+
+import android.app.Application;
+
+/**
+ * Created by Jakub Begera (jakub@easycoreapps.com) on 19.10.16.
+ */
+public class NomadeskApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
